53d8296919902d8ed18cc1f9b9795ab541001a48
GhifariArsa <s3893099@student.rmit.edu.au>
2023-07-02 23:57:47 +0700

    Finish

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
 function getComputerChoice () { 
-    let randomNumber = Math.floor(Math.random() * 3);
+    const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
-        case 1:
+        case 0:
             return "Rock";
             break;
-        case 2: 
+        case 1: 
             return "Scissors";
             break;
-        case 3:
+        case 2:
             return "Paper";
             break;
         default:
             break;
     }
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            return "Tie, both chose Rock.";
+        }
+        else if (computerSelection == "Scissors") {
+            return "Player Wins, Rock beats Scissors";
+        }
+        else {
+            return "Player Loses, Paper Beats Rock";
+        }
+    }
+    else if (playerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            return "Player Wins, Paper beats Rock";
+        }
+        else if (computerSelection == "Scissors") {
+            return "Player Loses, Scissors beats Paper";
+        }
+        else {
+            return "Tie, both chose Paper";
+        }
+    }
+    else if (playerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+            return "Player Loses, Rock beat Scissors";
+        }
+        else if (computerSelection == "Scissors") {
+            return "Tie, both chose scissors";
+        }
+        else {
+            return "Player Wins, Scissors beat Paper";
+        }
+    }
+}
+
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
