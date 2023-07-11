4eacc06b67e28c17fc2340ba5d913e79520c3f28
GhifariArsa <s3893099@student.rmit.edu.au>
2023-07-11 22:38:00 +0700

    finished the main meat of the website

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+function playGameRock() {
+    let computerSelection = getComputerChoice();
+    console.log(playGame(computerSelection, "Rock"));
+}
+
+function playGamePaper() {
+    let computerSelection = getComputerChoice();
+    console.log(playGame(computerSelection, "Paper"));
+}
+
+function playGameScissors() {
+    let computerSelection = getComputerChoice();
+    playGame(computerSelection, "Scissors");
+}
+
+function playGame(computerSelection, playerSelection) {
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            return "Tie, both chose Rock.";
+        }
+        else if (computerSelection == "Scissors") {
+            updateScoreHuman();
+        }
+        else {
+            updateScoreComputer();
+        }
+    }
+    else if (playerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            updateScoreHuman();
+        }
+        else if (computerSelection == "Scissors") {
+            updateScoreComputer();
+        }
+        else {
+            return "Tie, both chose Paper";
+        }
+    }
+    else if (playerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+            updateScoreComputer();
+        }
+        else if (computerSelection == "Scissors") {
+            
+        }
+        else {
+            updateScoreHuman();
+        }
+    }
+} 
+
 function getComputerChoice () { 
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -16,43 +67,16 @@ function getComputerChoice () {
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Rock") {
-        if (computerSelection == "Rock") {
-            return "Tie, both chose Rock.";
-        }
-        else if (computerSelection == "Scissors") {
-            return "Player Wins, Rock beats Scissors";
-        }
-        else {
-            return "Player Loses, Paper Beats Rock";
-        }
-    }
-    else if (playerSelection == "Paper") {
-        if (computerSelection == "Rock") {
-            return "Player Wins, Paper beats Rock";
-        }
-        else if (computerSelection == "Scissors") {
-            return "Player Loses, Scissors beats Paper";
-        }
-        else {
-            return "Tie, both chose Paper";
-        }
-    }
-    else if (playerSelection == "Scissors") {
-        if (computerSelection == "Rock") {
-            return "Player Loses, Rock beat Scissors";
-        }
-        else if (computerSelection == "Scissors") {
-            return "Tie, both chose scissors";
-        }
-        else {
-            return "Player Wins, Scissors beat Paper";
-        }
-    }
+function updateScoreHuman() {
+   let score = parseInt(document.getElementById("human-counter").innerHTML);
+   score += 1;
+   console.log(score);
+   document.getElementById("human-counter").innerHTML = score.toString();   
 }
 
-const playerSelection = "Paper";
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+function updateScoreComputer() {
+    let score = parseInt(document.getElementById("human-counter").innerHTML);
+    score += 1;
+    console.log(score);
+    document.getElementById("computer-counter").innerHTML = score.toString();  
+}
