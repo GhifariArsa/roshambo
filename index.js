6ff5271cb7d55fff5b5c8028d601d2249e3b2181
GhifariArsa <s3893099@student.rmit.edu.au>
2023-07-17 21:32:51 +1000

    created scorepop

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+let scorePop = document.getElementById('score');
+
+document.getElementById("rock").addEventListener('click', () => playGameRock());
+document.getElementById("paper").addEventListener('click', () => playGamePaper());
+document.getElementById("scissors").addEventListener('click', () => playGameScissors());
+
+
 function playGameRock() {
     let computerSelection = getComputerChoice();
-    console.log(playGame(computerSelection, "Rock"));
+    console.log(playGame(computerSelection, "ROCK"));
 }
 
 function playGamePaper() {
     let computerSelection = getComputerChoice();
-    console.log(playGame(computerSelection, "Paper"));
+    console.log(playGame(computerSelection, "PAPER"));
 }
 
 function playGameScissors() {
     let computerSelection = getComputerChoice();
-    playGame(computerSelection, "Scissors");
+    playGame(computerSelection, "SCISSORS");
 }
 
 function playGame(computerSelection, playerSelection) {
-    if (playerSelection == "Rock") {
-        if (computerSelection == "Rock") {
-            return "Tie, both chose Rock.";
-        }
-        else if (computerSelection == "Scissors") {
-            updateScoreHuman();
-        }
-        else {
-            updateScoreComputer();
-        }
+    let gameWinner = 'NONE';
+    if (playerSelection === computerSelection) {
+        gameWinner = "NONE"
     }
-    else if (playerSelection == "Paper") {
-        if (computerSelection == "Rock") {
-            updateScoreHuman();
-        }
-        else if (computerSelection == "Scissors") {
-            updateScoreComputer();
-        }
-        else {
-            return "Tie, both chose Paper";
-        }
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+        gameWinner = 'Player';
+        updateScoreHuman();
     }
-    else if (playerSelection == "Scissors") {
-        if (computerSelection == "Rock") {
-            updateScoreComputer();
-        }
-        else if (computerSelection == "Scissors") {
-            
-        }
-        else {
-            updateScoreHuman();
-        }
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ) {
+        gameWinner = 'Computer';
+        updateScoreComputer();
     }
+
+    updateScorePop(gameWinner, playerSelection, computerSelection);
 } 
 
 function getComputerChoice () { 
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-            return "Rock";
+            return "ROCK";
             break;
         case 1: 
-            return "Scissors";
+            return "SCISSORS";
             break;
         case 2:
-            return "Paper";
+            return "PAPER";
             break;
         default:
             break;
     }
 }
+
 
 
 function updateScoreHuman() {
@@ -75,8 +75,19 @@ function updateScoreHuman() {
 }
 
 function updateScoreComputer() {
-    let score = parseInt(document.getElementById("human-counter").innerHTML);
+    let score = parseInt(document.getElementById("computer-counter").innerHTML);
     score += 1;
     console.log(score);
     document.getElementById("computer-counter").innerHTML = score.toString();  
 }
+
+function updateScorePop(gameWinner, playerChoice, computerChoice) {
+    if (gameWinner == "NONE") {
+        scorePop.textContent = "It's a tie! Both chose " + playerChoice;
+    } else if (gameWinner == "Player") {
+        scorePop.textContent = "Player wins with: " + playerChoice;
+    } else {
+        scorePop.textContent = "Computer wins with: " + computerChoice;
+    }
+}
+
